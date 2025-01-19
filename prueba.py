b05924cc3c66bb4b44c440461ac7057ded30ea10
Suero <antonio.suero@hotmail.com>
2025-01-19 13:09:19 +0100

    Nuevo

--- prueba.py
+++
@@ -1,5 +0,0 @@
-print("Hola Git!")
-print("Bienvenido a MOUREDEVpro...")
-nombre = str(input("Escribe tu nombre: "))
-print(f"\n\nTe sentirás como en casa {nombre}")
-print("#######################################")
